206b1e4c0a3a8b5bcaee30ab24e738f8e43175a0
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-25 01:36:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ attr_accessor :name, :songs
 
 def initialize(name)
   @name = name 
-  @@all << self 
+save
 end
 
 def self.all 
@@ -25,5 +25,13 @@ end
 
 def self.find(name)
   self.all.find {|name| artist.name == name}
+end
+
+def save
+  @@all << self 
+end
+
+def print_songs
+  songs.each {|song| puts song.name}
 end
 end
